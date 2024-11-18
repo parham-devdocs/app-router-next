187b8e0a4955dfb5ef8a7ba1f7ddf6bfb0aee203
parham-devdocs <p.77.pazargadi@gmail.com>
2024-11-18 23:59:14 +0330

    classes applied

--- pages/components/event-item.js
+++ pages/components/event-item.js
@@ -1,24 +1,28 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import classes from "./event.item.module.css";
 const EventItem = ({ title, image, location, date, id }) => {
- const formattedAddress=location.replace(",","\n")
-    const humanReadableDate = new Date(date).toLocaleDateString("en-US", { day: "numeric", "month": "long", year: "numeric" })
+  const formattedAddress = location.replace(",", "\n");
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
   return (
-    <li>
+    <li className={classes.item}>
       <Image src={`/${image}`} alt={title} width={150} height={100} />
-      <div>
-        <div>
-                  <h2>{ title }</h2>
-          <div>
-                      <time>{ humanReadableDate }</time>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+          <div className={classes.date}>
+            <time>{humanReadableDate}</time>
           </div>
-          <div>
-                      <address>{ formattedAddress}</address>
+          <div className={classes.address}>
+            <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={`/evnets/${id}`}>Explore Event</Link>
         </div>
       </div>
--- pages/components/eventList.js
+++ pages/components/eventList.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import EventItem from "./event-item";
+import classes from "./event.list.module.css";
 const EventList = ({items}) => {
   return (
-    <ul>
+    <ul className={classes.list}>
       {items.map(event=>{return <EventItem id={event.id} image={event.image} title={event.title} date={event.date} location={event.location}/>})}
     </ul>
   )
