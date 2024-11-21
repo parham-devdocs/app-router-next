cc21e3f508df6a158e020aa15431d3f36c3afdd6
parham-devdocs <p.77.pazargadi@gmail.com>
2024-11-21 20:01:11 +0330

    get special event page completed

--- pages/events/[...date].js
+++ pages/events/[...date].js
@@ -1,10 +1,27 @@
 import { useRouter } from "next/router";
 import React from "react";
+import { getFilteredEvents } from "../../data/DummyData";
+import EventList from "../../components/eventList";
 
 const SpecialEvent = () => {
-  const router = useRouter();
-  console.log(router.query);
-  return <div>special event</div>;
+
+  const router=useRouter()
+  const date = router.query.date;
+  if (!date) {
+    return <p className="center">Loading</p>;
+  }
+  const year = Number(date[0]);
+  const month = Number(date[1])
+  if (isNaN(year)|| isNaN(month)|| year<2021 || year>2030|| month<1|| month>12) {
+    return <p className=" center">invalid filter</p>
+  }
+  const filteredDate={year,month}
+  const filteredEvent = getFilteredEvents(filteredDate)
+  if (filteredEvent.length === 0) {
+    setTimeout(()=>{router.replace("/events")},3000)
+    return <p className=" center">no event found</p>
+  }
+  return <div><EventList items={filteredEvent}/></div>;
 };
 
 export default SpecialEvent;
